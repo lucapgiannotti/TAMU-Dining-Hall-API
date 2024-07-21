f73dd36cf51206186091a9e54222da542fa1c9ae
Luca Giannotti <dev@lucagiannotti.com>
2024-07-20 23:22:14 -0500

    fix: headless mode now works with button press

--- web_scraper.py
+++ web_scraper.py
@@ -13,6 +13,8 @@ def web_scraper(button_id):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("window-size=1920x1080")
+
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     driver.get(url)
