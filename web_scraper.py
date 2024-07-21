4906994ac6714d5e9e2bf606320629350561029f
Luca Giannotti <dev@lucagiannotti.com>
2024-07-20 20:31:37 -0500

    prod: added chrome_options

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,5 @@
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.chrome.service import Service as ChromeService 
 from webdriver_manager.chrome import ChromeDriverManager 
@@ -8,7 +9,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def get_menu():
     url = 'https://dineoncampus.com/tamu/whats-on-the-menu/' 
-    driver = webdriver.Chrome() 
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     driver.get(url)
     wait = WebDriverWait(driver, 20).until(
