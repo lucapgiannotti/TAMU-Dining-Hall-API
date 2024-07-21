5f5a6a10ced83f6c303aa6f950624fc05b73f121
Luca Giannotti <dev@lucagiannotti.com>
2024-07-20 23:02:07 -0500

    feat: lunch and dinner menus for commons hall

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import subprocess
-from web_scraper import get_menu
+from web_scraper import get_breakfast_menu, get_lunch_menu, get_dinner_menu
 
 app = Flask(__name__)
 
@@ -15,7 +15,17 @@ def get_test():
 
 @app.route('/breakfast_menu', methods=['GET'])
 def get_breakfast_menu():
-    result = get_menu()
+    result = get_breakfast_menu()
+    return jsonify({'menu_items': result})
+
+@app.route('/lunch_menu', methods=['GET'])
+def get_lunch_menu():
+    result = get_lunch_menu()
+    return jsonify({'menu_items': result})
+
+@app.route('/dinner_menu', methods=['GET'])
+def get_dinner_menu():
+    result = get_dinner_menu()
     return jsonify({'menu_items': result})
 
 if __name__ == '__main__':
--- web_scraper.py
+++ web_scraper.py
@@ -6,10 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-def get_menu():
-    url = 'https://dineoncampus.com/tamu/whats-on-the-menu/' 
-
+def web_scraper(button_id):
+    url = 'https://dineoncampus.com/tamu/whats-on-the-menu/'
+    
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -17,7 +16,15 @@ def get_menu():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     driver.get(url)
-    wait = WebDriverWait(driver, 20).until(
+     
+    wait = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, button_id))  
+    )
+    
+    button = driver.find_element(By.ID, button_id)
+    button.click()
+    
+    wait = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "menu-items"))
     )
     menu = driver.find_elements(By.CLASS_NAME, 'menu-items') 
@@ -27,3 +34,12 @@ def get_menu():
         for item in items:
             menu_items.append(item.text)
     return menu_items
+
+def get_breakfast_menu():
+    return web_scraper("__BVID__217___BV_tab_button__")
+
+def get_lunch_menu():
+    return web_scraper("__BVID__222___BV_tab_button__")
+
+def get_dinner_menu():
+    return web_scraper("__BVID__227___BV_tab_button__")
